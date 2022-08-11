0f43b54baae8a9efd96ee69da4f7f0fd43bb5283
Bamjong <qjawhd100@naver.com>
2022-08-12 03:56:06 +0900

    2022-08-12/AM_03_55

--- 3.반복문/3.합_8393/Main.java
+++ 3.반복문/3.합_8393/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int value = sc.nextInt();
+        int result = 0;
+
+        for (int i = 0; i <= value; i++) {
+            result += i;
+        }
+
+        System.out.println(result);
+
+        sc.close();
     }
 }
