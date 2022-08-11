3d6358525b3b810a5d8e1633977118c4d582df2d
Bamjong <ebune123@naver.com>
2022-08-11 19:26:04 +0900

    2022-08-11/PM_19_26

--- 3.반복문/1.구구단_2739/Main.java
+++ 3.반복문/1.구구단_2739/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         
+        int gugudan = sc.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.printf("%d * %d = %d \n", gugudan, i, gugudan*i);
+        }
+
+        sc.close();
     }
 }
--- 3.반복문/2.A+B-3_10950/Main.java
+++ 3.반복문/2.A+B-3_10950/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int count = sc.nextInt();
+
+        int first,second;
+
+        for (int i = 0; i < count; i++) {
+            first = sc.nextInt();
+            second = sc.nextInt();
+            System.out.println(first + second);
+        }
         
+        sc.close();
     }
 }
