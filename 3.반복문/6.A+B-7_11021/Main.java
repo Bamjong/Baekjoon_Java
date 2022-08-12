97831b43dbb8a160ece1d7a0f0ce9fdee42205d8
Bamjong <ebune123@naver.com>
2022-08-12 19:38:50 +0900

    2022-08-12/PM_19_38

--- 3.반복문/6.A+B-7_11021/Main.java
+++ 3.반복문/6.A+B-7_11021/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number_length = sc.nextInt();
+        int result = 0;
+
+        for (int i = 0; i < number_length; i++) {
+            result = plus(sc.nextInt(), sc.nextInt());
+            System.out.println("Case #"+(i+1)+": "+ result);
+        }
+
+        sc.close();
         
     }
+    public static int plus(int a, int b) {
+        int c = a + b;
+
+        return c;
+    }
 }
+
+
+
+
+
+// 5
+// 1 1
+// 2 3
+// 3 4
+// 9 8
+// 5 2
+
+// Case #1: 2
+// Case #2: 5
+// Case #3: 7
+// Case #4: 17
+// Case #5: 7
+
