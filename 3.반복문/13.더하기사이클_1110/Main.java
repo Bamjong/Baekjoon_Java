1f3b26a0ef89c347334e9f6db4752473c1d315c4
Bamjong <qjawhd100@naver.com>
2022-08-23 18:58:29 +0900

    완료,,, 했지만 잘모르겟음

--- 3.반복문/13.더하기사이클_1110/Main.java
+++ 3.반복문/13.더하기사이클_1110/Main.java
@@ -3,22 +3,19 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-    
-        int first = sc.nextInt();
-        int tens,units,add,result;
-        int i = 0;
+        int count = 0; //카운트
+        int receive = sc.nextInt();
+        int receive_first = receive;
 
-        int first1 = first;
-        
-        tens = first / 10;
-        units = first - (tens * 10);
-        add = tens + units;
-        
-
-
-        (units * 10) + add;
-
-        System.out.println(tens +  " "  + units + " " + add);
-        
+        while (true) {
+            receive = ((receive % 10) * 10) + (((receive / 10) + (receive % 10)) % 10);  // 2 + ((2+6)%10)
+            count++;
+            if (receive_first == receive) {
+                break;
+            }
+        }
+        System.out.println(count);
+        sc.close();
     }
+
 }
